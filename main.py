64bccab9f21df42d97c6bad4af69003d3c771abb
Aliah Ojendyk <30396713+aliahshea@users.noreply.github.com>
2018-05-25 21:11:56 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def thankyou():
+    print("Thank you for the Echo Dot, I’ve enjoyed it immensely!")
+    print("I feel honored that I’ve gotten so many opportunities through you ")
+    print("and your faith in me. I’m sure I wouldn’t have been able to get")
+    print("where I am today without your help and it’s meant a lot. ")
+    print("You have inspired me to keep learning and growing in anything ")
+    print("CS related. I seriously wish I could stay to see how you expand the program.")
+
+name = input("Who would you like to say thank you to? ")
+yourname = input("What is your name? ")
+
+print("")
+print(str(name) + ", ")
+print("")
+thankyou()
+print("")
+print("Best wishes, " )
+print(yourname)
